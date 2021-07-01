d31abf48be145008fe3b6a07a89527d7fef6be09
Nassif <nassif.najdat@gmail.com>
2021-07-01 20:14:18 +0200

    some

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
+const WithWorker = () => {
   const [count, setCount] = React.useState(0);
   const [lazyCount, setLazyCount] = React.useState(0);
   const worker: Worker = React.useMemo(() => new Worker("worker.js"), []);
@@ -14,15 +14,54 @@ export default function App() {
     };
   }, [worker]);
 
-  function increase() {
+  const increase = () => {
     worker.postMessage({ msg: "increase", count: lazyCount });
-  }
+  };
 
   return (
     <div className="App">
+      <h2>With Worker</h2>
+      <p>
+        The UI will still be responding as expedcted since we moved the running
+        process from the main app to the worker
+      </p>
       <button onClick={() => setCount(count + 1)}>count {count}</button>
       <br />
       <button onClick={() => increase()}>lazy count {lazyCount}</button>
+    </div>
+  );
+};
+
+const WithOutWorker = () => {
+  const [count, setCount] = React.useState(0);
+  const [lazyCount, setLazyCount] = React.useState(0);
+
+  const increase = () => {
+    const start = Date.now();
+    while (Date.now() < start + 3000) {}
+    setLazyCount(lazyCount + 1);
+  };
+
+  return (
+    <div className="App">
+      <h2>With out Worker</h2>
+      <p>
+        The whole UI will be blocked without the worker the app will not
+        responed unless the running operation is finished
+      </p>
+      <button onClick={() => setCount(count + 1)}>count {count}</button>
+      <br />
+      <button onClick={() => increase()}>lazy count {lazyCount}</button>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <WithWorker />
+      <hr />
+      <WithOutWorker />
     </div>
   );
 }
